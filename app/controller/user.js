eb4c05fe29920cd86bb004b07f7d8ecdd06d005a
Magin-CC <573746388@qq.com>
2019-04-30 11:11:43 +0800

    注册发送邮件

--- app/controller/user.js
+++ app/controller/user.js
@@ -3,7 +3,7 @@
 const Controller = require('egg').Controller;
 class UserController extends Controller {
   async create() {
-    const ctx = this.ctx;
+    const { ctx, app } = this;
     // 校验 `ctx.request.body` 是否符合我们预期的格式
     // 如果参数校验未通过，将会抛出一个 status = 422 的异常
     // 调用 service 创建一个 topic
@@ -12,7 +12,11 @@ class UserController extends Controller {
       ctx.body = '';
       return;
     }
-    await ctx.service.user.create(ctx.request.body);
+    const data = ctx.request.body;
+    await ctx.service.user.create(data);
+    app.email.sendEmail('【网络医院广告系统】账号申请成功',
+      `账号：${data.username}\n密码：${data.password}\n登录系统后请修改密码！`,
+      data.email);
     // 设置响应体和状态码
     ctx.body = {
       status: 1,
